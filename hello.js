e7d1c5360e77ddb05bc74f1c8a093248898300bb
Victor Welson <welsonvictorskie@gmail.com>
2026-09-25 06:10:52 +0100

    change the funtion greeet to greet and change greeting in funtion to welcome

--- hello.js
+++ hello.js
@@ -1,10 +1,10 @@
 const name = "Welson";
-console.log("welcome, " + name + "! Node is working.");
+console.log("Welcome, " + name + "! Node is working.");
 console.log("Learning web development, one step at a time.");
 
 // Function that builds a greeting string for a given name
-function greeet(name) {
-  return "Hello, " + name + "!";
+function greet(name) {
+  return "Welcome, " + name + "!";
 }
 
 // List of names to greet
@@ -12,5 +12,5 @@ const names = ["Ana", "Bilal", "Chen", "temz"];
 
 // Loop through the names and print a greeting for each one
 for (const n of names) {
-  console.log(greeet(n));
+  console.log(greet(n));
 }
